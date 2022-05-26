43bd93fc1fb5ede286c2a78325879e6d9c261e3a
Erika Teal <erikamw2070@yahoo.com>
2022-05-26 11:56:24 -0500

    cleaned up semicolons

--- js/main.js
+++ js/main.js
@@ -5,23 +5,23 @@ let productArr = [];
 let productName = [];
 let productImage = [];
 let productLink = [];
-const url = `https://makeup-api.herokuapp.com/api/v1/products.json`
+const url = `https://makeup-api.herokuapp.com/api/v1/products.json`;
 
 fetch(url)
   .then(res => res.json()) //parse response as JSON
   .then(data => {
     // console.log(data)
     for(let i = 0; i < data.length; i++){
-      productArr.push(data[i].product_type) // pushing every element in the product type to an array
+      productArr.push(data[i].product_type); // pushing every element in the product type to an array
     }
     let uniqueProduct = [...new Set(productArr)]; // creating a new set w/ just the unique values from the array
     uniqueProduct.map(product => {
       product = product[0].toUpperCase() + product.slice(1);
       let formattedProduct = product.replace('_', ' ');
-      const option = document.createElement('option') // <option></option>
-      option.value = product // <option value ='product'></option>
-      option.innerText = formattedProduct // <option value ='product'>Product</option>
-      selectProduct.appendChild(option)
+      const option = document.createElement('option'); // <option></option>
+      option.value = product; // <option value ='product'></option>
+      option.innerText = formattedProduct; // <option value ='product'>Product</option>
+      selectProduct.appendChild(option);
     })
   //   document.querySelector('.previous').addEventListener('click', previous)
 	// function previous(e){
@@ -44,22 +44,22 @@ fetch(url)
 	// };
   })
   .catch(err => {
-    console.log(`error ${err}`)
+    console.log(`error ${err}`);
   });
 
 const selectProduct = document.querySelector('.productType');
 selectProduct.addEventListener('change', event => {
-  product = event.target.value
+  product = event.target.value;
   urlChoice = `https://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product}`;
   // console.log(product);
 })
 
 document.querySelector('.submit').addEventListener('click', function (){
-  getProductImage(product, urlChoice)
-  document.querySelector('.image').classList.remove('hidden')
-  document.querySelector('h4').classList.add('hidden')
-  document.querySelector('.previous').classList.remove('hidden')
-  document.querySelector('.next').classList.remove('hidden')
+  getProductImage(product, urlChoice);
+  document.querySelector('.image').classList.remove('hidden');
+  document.querySelector('h4').classList.add('hidden');
+  document.querySelector('.previous').classList.remove('hidden');
+  document.querySelector('.next').classList.remove('hidden');
 })
 
 const getProductImage = (value, urlChoice) => {
@@ -67,9 +67,9 @@ const getProductImage = (value, urlChoice) => {
   .then(res => res.json()) //parse response as JSON
   .then(data => {
     for(let i = 0; i < data.length; i++){
-      document.querySelector('.productName').innerText = data[i].name
-      document.querySelector('.productImage').src = data[i].image_link
-      document.querySelector('.productLink').href = data[i].product_link
+      document.querySelector('.productName').innerText = data[i].name;
+      document.querySelector('.productImage').src = data[i].image_link;
+      document.querySelector('.productLink').href = data[i].product_link;
       // productName.push(data[i].name)
       // productImage.push(data[i].image_link)
       // productLink.push(data[i].product_link)
