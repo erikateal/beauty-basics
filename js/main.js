f3c1714300d22a927930989f3b0a1e8c63d169e3
Erika Teal <erikamw2070@yahoo.com>
2022-05-26 11:53:43 -0500

    refactoring code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 let product;
 let urlChoice;
+let count;
+let productArr = [];
+let productName = [];
+let productImage = [];
+let productLink = [];
 const url = `https://makeup-api.herokuapp.com/api/v1/products.json`
 
 fetch(url)
   .then(res => res.json()) //parse response as JSON
   .then(data => {
-    let array = [];
+    // console.log(data)
     for(let i = 0; i < data.length; i++){
-      array.push(data[i].product_type) // pushing every element in the product type to an array
+      productArr.push(data[i].product_type) // pushing every element in the product type to an array
     }
-    let uniqueProduct = [...new Set(array)]; // creating a new set w/ just the unique values from the array
+    let uniqueProduct = [...new Set(productArr)]; // creating a new set w/ just the unique values from the array
     uniqueProduct.map(product => {
       product = product[0].toUpperCase() + product.slice(1);
       let formattedProduct = product.replace('_', ' ');
@@ -18,6 +23,25 @@ fetch(url)
       option.innerText = formattedProduct // <option value ='product'>Product</option>
       selectProduct.appendChild(option)
     })
+  //   document.querySelector('.previous').addEventListener('click', previous)
+	// function previous(e){
+	// 	e.preventDefault();
+	// 	count--
+	// 	if (count < 0) count = data.length - 1;
+	// 	document.querySelector('h2').innerText = productName[count]
+	// 	document.querySelector('img').src = productImage[count]
+	// 	document.getElementsByClassName('productLink').innerText = productLink[count]
+	// };
+
+	// document.querySelector('.next').addEventListener('click', next)
+	// function next(e){
+	// 	e.preventDefault();
+	// 	count++
+	// 	if (count > data.length - 1) count = 0;
+	// 	document.querySelector('h2').innerText = productName[count]
+	// 	document.querySelector('img').src = productImage[count]
+	// 	document.getElementsByClassName('productLink').innerText = productLink[count]
+	// };
   })
   .catch(err => {
     console.log(`error ${err}`)
@@ -27,7 +51,7 @@ const selectProduct = document.querySelector('.productType');
 selectProduct.addEventListener('change', event => {
   product = event.target.value
   urlChoice = `https://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product}`;
-  console.log(product);
+  // console.log(product);
 })
 
 document.querySelector('.submit').addEventListener('click', function (){
@@ -46,44 +70,20 @@ const getProductImage = (value, urlChoice) => {
       document.querySelector('.productName').innerText = data[i].name
       document.querySelector('.productImage').src = data[i].image_link
       document.querySelector('.productLink').href = data[i].product_link
+      // productName.push(data[i].name)
+      // productImage.push(data[i].image_link)
+      // productLink.push(data[i].product_link)
     }
+    // let random = Math.floor(Math.random() * productName.length)
+    // document.querySelector('.productName').innerText = productName[random]
+    // document.querySelector('.productImage').src = productImage[random]
+    // document.querySelector('.productLink').href = productLink[random]
+    // console.log(random)
   })
+  // console.log(productName)
+  // console.log(productImage)
+  // console.log(productLink)
 }
-
-
-// document.querySelector('.previous').addEventListener('click', previous)
-
-// 	function previous(e){
-// 		e.preventDefault();
-// 		count--
-// 		if (count < 0) count = data.length - 1;
-// 		document.querySelector('h2').innerText = data[count].name
-// 		document.querySelector('img').src = data[count].image_link
-// 		document.querySelector('p').innerText = data[count].product_link
-// 	};
-
-// 	document.querySelector('.next').addEventListener('click', next)
-
-// 	function next(e){
-// 		e.preventDefault();
-// 		count++
-// 		if (count > data.length - 1) count = 0;
-// 		document.querySelector('h2').innerText = data[count].name
-// 		document.querySelector('img').src = data[count].image_link
-// 		document.querySelector('p').innerText = data[count].product_link
-// 	};
-
 
 // Makeup API link: https://www.programmableweb.com/api/makeup-rest-api-v10
 // Documentation: https://makeup-api.herokuapp.com/
-
-const url1 = `https://makeup-api.herokuapp.com/api/v1/products.json`
-
-fetch(url1)
-.then(res => res.json()) // parse response as JSON
-.then(data => {
-  console.log(data)
-})
-.catch(err => {
-    console.log(`error ${err}`)
-});
